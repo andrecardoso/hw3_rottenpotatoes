db00625838f0ccc7fef808d13247ba78fc8b31ad
Andre Luiz Cardoso <andrecardoso@gmail.com>
2013-02-10 16:42:02 -0800

    Addins step definition to check/uncheck ratings

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -19,8 +19,8 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|  
+  rating_list.split.each do |rating|
+    step "I #{uncheck} #{rating}"
+  end
 end
